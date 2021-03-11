0e485c2b746a08ff38953f15984727f5391880dc
cristinaivan98 <ivancristina18@stud.ase.ro>
2021-03-11 18:06:25 +0300

    seminar3 faza 1

--- Seminar2/src/ro/ase/cts/clase/readers/IReader.java
+++ Seminar2/src/ro/ase/cts/clase/readers/IReader.java
@@ -1,8 +1,34 @@
 package ro.ase.cts.clase.readers;
+
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 import ro.ase.cts.clase.Aplicant;
-public interface IReader {
-	public List<Aplicant> readAplicants(String file)throws FileNotFoundException, NumberFormatException;
+public abstract  class IReader {
+	protected String filePath;
+	
+	public IReader(String filePath) {
+		super();
+		this.filePath = filePath;
+	}
+	public abstract List<Aplicant> readAplicants()throws FileNotFoundException, NumberFormatException;
+	public void citireAplicant(Scanner input,Aplicant aplicant) {
+		String nume = input.next();
+		String prenume = input.next();
+		int varsta = input.nextInt();
+		int punctaj = input.nextInt();
+		int nr = input.nextInt();
+		String[] vect = new String[nr];
+		for (int i = 0; i < nr; i++)
+			vect[i] = input.next();
+		//clasa Aplicant e abstracta deci nu poate fi instantiata
+		aplicant.setNume(nume);
+		aplicant.setPrenume(prenume);
+		aplicant.setVarsta(varsta);
+		aplicant.setPunctaj(punctaj);
+		aplicant.setNr_proiecte(nr);
+		aplicant.setVectorDenumiri(nr, vect);
+		//return aplicant;
+	}
 }
--- Seminar2/src/ro/ase/cts/program/Program.java
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -5,18 +5,24 @@ package ro.ase.cts.program;
 import java.io.FileNotFoundException;
 import java.util.List;
 import ro.ase.cts.clase.readers.AngajatiReader;
-
-
+import ro.ase.cts.clase.readers.IReader;
 import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
 
 public class Program {
+	
+	public static List<Aplicant> citesteAplicanti(IReader readAplicant) throws FileNotFoundException{
+			return readAplicant.readAplicants();
+		
+		
+	}
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAplicanti;
 		try {
-			listaAngajati = AngajatiReader.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
-				System.out.println(angajat.toString());
+			listaAplicanti = citesteAplicanti( new AngajatiReader("angajati.txt"));
+			for(Aplicant aplicant:listaAplicanti)
+				System.out.println(aplicant.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
