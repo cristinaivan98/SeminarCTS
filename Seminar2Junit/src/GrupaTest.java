0ca3940c1a050fda16231be5f75857cd849af890
cristinaivan98 <ivancristina18@stud.ase.ro>
2021-05-28 15:29:27 +0300

    junit2 parte2

--- Seminar2Junit/src/GrupaTest.java
+++ Seminar2Junit/src/GrupaTest.java
@@ -1,10 +1,13 @@
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class GrupaTest {
+
     @Test
+
     public void testConstructorNrGrupaCorect(){
         Grupa grupa=new Grupa(1081);
         assertEquals(1081,grupa.getNrGrupa());
@@ -39,6 +42,64 @@ public class GrupaTest {
     @Test(timeout = 500)
     public void testPerformance(){
         Grupa grupa=new Grupa(1055);
+    }
+
+    @Test
+    public void testPromovabilitateRight(){
+        Grupa grupa=new Grupa(1081);
+        for(int i=0;i<10;i++){
+            IStudent student=new Student();
+            student.adaugaNota(5);
+            student.adaugaNota(7);
+            grupa.adaugaStudent(student);
+        }
+        for(int i=0;i<5;i++){
+            IStudent student=new Student();
+            student.adaugaNota(4);
+            student.adaugaNota(7);
+            grupa.adaugaStudent(student);
+        }
+        assertEquals(0.66,grupa.getPromovabilitate(),0.1);
+
+
+    }
+    @Test
+    public void testGetPRomovabilitateInferioara(){
+        Grupa grupa=new Grupa(1001);
+        for(int i=0;i<5;i++){
+            IStudent student=new Student();
+            student.adaugaNota(4);
+            student.adaugaNota(7);
+            grupa.adaugaStudent(student);
+        }
+        assertEquals(0,grupa.getPromovabilitate(),0.1);
+    }
+    @Test
+    public void testGetPromovabilitateLimitaSuperioara(){
+        Grupa grupa=new Grupa(1100);
+        for(int i=0;i<10;i++){
+            IStudent student=new Student();
+            student.adaugaNota(7);
+            student.adaugaNota(5);
+            grupa.adaugaStudent(student);
+        }
+        assertEquals(1,grupa.getPromovabilitate(),0.1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetPromovabilitateErrorCondition(){
+        Grupa grupa=new Grupa(1050);
+       grupa.getPromovabilitate();
+    }
+    //CORECT
+    @Test
+    public void testGetPromovabilitateCArdinality1(){
+        Grupa grupa=new Grupa(1081);
+        IStudent student=new Student();
+        student.adaugaNota(8);
+        student.adaugaNota(10);
+        grupa.adaugaStudent(student);
+        assertEquals(1,grupa.getPromovabilitate(),0.1);
     }
 
 
