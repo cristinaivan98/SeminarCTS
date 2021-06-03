fb5dd3dd64580233949ec27234575b4991468fe7
cristinaivan98 <ivancristina18@stud.ase.ro>
2021-06-03 18:30:48 +0300

    Mock/Fake/Stub

---
+++ Seminar2Junit/src/StubStudent.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+public class StubStudent implements IStudent {
+    @Override
+    public String getNume() {
+        return "Gigel";
+    }
+
+    @Override
+    public void setNume(String nume) {
+
+    }
+
+    @Override
+    public List<Integer> getNote() {
+        return null;
+    }
+
+    @Override
+    public void adaugaNota(int nota) {
+
+    }
+
+    @Override
+    public float calculeazaMedie() {
+        return 6.7f;
+    }
+
+    @Override
+    public int getNota(int index) {
+        return 9;
+    }
+
+    @Override
+    public boolean areRestante() {
+        return false;
+    }
+}
